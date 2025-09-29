f82835910c5721290d86ff677882c4acab0fccdb
Ryan Adams <rjadams82@gmail.com>
2025-09-29 10:51:48 -0400

    Update ovpn-ptp-fix.sh
    variable quotes

--- ovpn-ptp-fix.sh
+++ ovpn-ptp-fix.sh
@@ -18,7 +18,7 @@ cfgexp='peer.config.*'                  # config file pattern to match
 #
 # NO MORE EDITS BELOW #
 /usr/bin/logger -t "$logtag" -p 6 -- 'start script'
-if [ $(ls -1 $cfgdir$cfgexp | wc -l) -gt 0 ]; then
+if [ $(ls -1 "$cfgdir"$cfgexp | wc -l) -gt 0 ]; then
     # found the file(s)
     for file in "$cfgdir"$cfgexp; do
         scount=0
